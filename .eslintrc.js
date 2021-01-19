e67c585aa5afe0158360d5cfccac6bb3033eadc9
adriandobrzynski <adrian.dobrzynski93@gmail.com>
2021-01-19 12:53:07 +0100

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  root: true,
+  extends: [
+    'satya164',
+    'plugin:react-native/all',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+};
